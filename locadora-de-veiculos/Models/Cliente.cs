649549de3e451d2d17d6f94dccf8025890f56e6d
robsonosorio <robson.osorioo@gmail.com>
2020-07-01 17:45:41 -0300

    criado primeira view de cadadstro de cliente

---
+++ locadora-de-veiculos/Models/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace locadora_de_veiculos.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int CPF { get; set; }
+        public int Telefone { get; set; }
+
+        public Cliente()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome
+                + "  Email: " + Email
+                + "\nCPF: " + CPF
+                + "  Tel.: " + Telefone;
+        }
+    }
+}
---
+++ locadora-de-veiculos/Views/Cliente/NovoCadastro.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+using locadora_de_veiculos.Models;
+
+namespace locadora_de_veiculos.Views.Cliente
+{ 
+    public partial class NovoCadastro : Form
+    {
+        Models.Cliente cliente = new Models.Cliente();
+        public NovoCadastro()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             cliente.Nome = textBox1.Text;
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            cliente.Email = textBox2.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            cliente.CPF = int.Parse(textBox3.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            cliente.Telefone = int.Parse(textBox4.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Cadastro realizado com sucesso!\n"
+                + cliente );
+        }
+    }
+}
