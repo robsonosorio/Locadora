c787f3f387edd7748a84778871ca7c77a2e329c3
robsonosorio <robson.osorioo@gmail.com>
2020-07-05 19:01:02 -0300

    alterado o projeto para .NET Framework / CRUD clientes ok

---
+++ Locadora/FormCliente.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace Locadora
+{
+    partial class FormCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label telefoneLabel;
+            System.Windows.Forms.Label cPFLabel;
+            System.Windows.Forms.Label emailLabel;
+            System.Windows.Forms.Label nomeLabel;
+            this.telefoneTextBox = new System.Windows.Forms.TextBox();
+            this.cPFTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.nomeTextBox = new System.Windows.Forms.TextBox();
+            this.clienteDataGridView = new System.Windows.Forms.DataGridView();
+            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.locadoraDataSet = new Locadora.LocadoraDataSet();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clienteTableAdapter = new Locadora.LocadoraDataSetTableAdapters.ClienteTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nomeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cPFDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.telefoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            telefoneLabel = new System.Windows.Forms.Label();
+            cPFLabel = new System.Windows.Forms.Label();
+            emailLabel = new System.Windows.Forms.Label();
+            nomeLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // telefoneLabel
+            // 
+            telefoneLabel.AutoSize = true;
+            telefoneLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            telefoneLabel.Location = new System.Drawing.Point(283, 91);
+            telefoneLabel.Name = "telefoneLabel";
+            telefoneLabel.Size = new System.Drawing.Size(26, 15);
+            telefoneLabel.TabIndex = 39;
+            telefoneLabel.Text = "Tel.";
+            // 
+            // cPFLabel
+            // 
+            cPFLabel.AutoSize = true;
+            cPFLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            cPFLabel.Location = new System.Drawing.Point(283, 54);
+            cPFLabel.Name = "cPFLabel";
+            cPFLabel.Size = new System.Drawing.Size(27, 15);
+            cPFLabel.TabIndex = 37;
+            cPFLabel.Text = "CPF";
+            // 
+            // emailLabel
+            // 
+            emailLabel.AutoSize = true;
+            emailLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            emailLabel.Location = new System.Drawing.Point(14, 90);
+            emailLabel.Name = "emailLabel";
+            emailLabel.Size = new System.Drawing.Size(36, 15);
+            emailLabel.TabIndex = 35;
+            emailLabel.Text = "Email";
+            // 
+            // nomeLabel
+            // 
+            nomeLabel.AutoSize = true;
+            nomeLabel.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            nomeLabel.Location = new System.Drawing.Point(12, 54);
+            nomeLabel.Name = "nomeLabel";
+            nomeLabel.Size = new System.Drawing.Size(41, 15);
+            nomeLabel.TabIndex = 33;
+            nomeLabel.Text = "Nome";
+            // 
+            // telefoneTextBox
+            // 
+            this.telefoneTextBox.Location = new System.Drawing.Point(316, 88);
+            this.telefoneTextBox.Name = "telefoneTextBox";
+            this.telefoneTextBox.Size = new System.Drawing.Size(125, 20);
+            this.telefoneTextBox.TabIndex = 41;
+            // 
+            // cPFTextBox
+            // 
+            this.cPFTextBox.Location = new System.Drawing.Point(316, 52);
+            this.cPFTextBox.Name = "cPFTextBox";
+            this.cPFTextBox.Size = new System.Drawing.Size(125, 20);
+            this.cPFTextBox.TabIndex = 40;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(59, 88);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(216, 20);
+            this.emailTextBox.TabIndex = 38;
+            // 
+            // nomeTextBox
+            // 
+            this.nomeTextBox.Location = new System.Drawing.Point(59, 54);
+            this.nomeTextBox.Name = "nomeTextBox";
+            this.nomeTextBox.Size = new System.Drawing.Size(216, 20);
+            this.nomeTextBox.TabIndex = 36;
+            // 
+            // clienteDataGridView
+            // 
+            this.clienteDataGridView.AllowUserToAddRows = false;
+            this.clienteDataGridView.AllowUserToDeleteRows = false;
+            this.clienteDataGridView.AutoGenerateColumns = false;
+            this.clienteDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.clienteDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nomeDataGridViewTextBoxColumn,
+            this.emailDataGridViewTextBoxColumn,
+            this.cPFDataGridViewTextBoxColumn,
+            this.telefoneDataGridViewTextBoxColumn});
+            this.clienteDataGridView.DataSource = this.clienteBindingSource;
+            this.clienteDataGridView.Location = new System.Drawing.Point(17, 131);
+            this.clienteDataGridView.Name = "clienteDataGridView";
+            this.clienteDataGridView.ReadOnly = true;
+            this.clienteDataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.clienteDataGridView.Size = new System.Drawing.Size(551, 261);
+            this.clienteDataGridView.TabIndex = 34;
+            this.clienteDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.clienteDataGridView_CellContentClick);
+            // 
+            // clienteBindingSource
+            // 
+            this.clienteBindingSource.DataMember = "Cliente";
+            this.clienteBindingSource.DataSource = this.locadoraDataSet;
+            // 
+            // locadoraDataSet
+            // 
+            this.locadoraDataSet.DataSetName = "LocadoraDataSet";
+            this.locadoraDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 30);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Cadastro de  clientes";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(584, 343);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 49);
+            this.button4.TabIndex = 31;
+            this.button4.Text = "Deletar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.deleta_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(584, 273);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 49);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Alterar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.altera_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(584, 202);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 49);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.salva_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(584, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 49);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Atualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.atualiza_Click);
+            // 
+            // clienteTableAdapter
+            // 
+            this.clienteTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // nomeDataGridViewTextBoxColumn
+            // 
+            this.nomeDataGridViewTextBoxColumn.DataPropertyName = "Nome";
+            this.nomeDataGridViewTextBoxColumn.HeaderText = "Nome";
+            this.nomeDataGridViewTextBoxColumn.Name = "nomeDataGridViewTextBoxColumn";
+            this.nomeDataGridViewTextBoxColumn.ReadOnly = true;
+            this.nomeDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // emailDataGridViewTextBoxColumn
+            // 
+            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
+            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
+            this.emailDataGridViewTextBoxColumn.ReadOnly = true;
+            this.emailDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // cPFDataGridViewTextBoxColumn
+            // 
+            this.cPFDataGridViewTextBoxColumn.DataPropertyName = "CPF";
+            this.cPFDataGridViewTextBoxColumn.HeaderText = "CPF";
+            this.cPFDataGridViewTextBoxColumn.Name = "cPFDataGridViewTextBoxColumn";
+            this.cPFDataGridViewTextBoxColumn.ReadOnly = true;
+            this.cPFDataGridViewTextBoxColumn.Width = 80;
+            // 
+            // telefoneDataGridViewTextBoxColumn
+            // 
+            this.telefoneDataGridViewTextBoxColumn.DataPropertyName = "Telefone";
+            this.telefoneDataGridViewTextBoxColumn.HeaderText = "Telefone";
+            this.telefoneDataGridViewTextBoxColumn.Name = "telefoneDataGridViewTextBoxColumn";
+            this.telefoneDataGridViewTextBoxColumn.ReadOnly = true;
+            this.telefoneDataGridViewTextBoxColumn.Width = 80;
+            // 
+            // FormCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 409);
+            this.Controls.Add(telefoneLabel);
+            this.Controls.Add(this.telefoneTextBox);
+            this.Controls.Add(cPFLabel);
+            this.Controls.Add(this.cPFTextBox);
+            this.Controls.Add(emailLabel);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(nomeLabel);
+            this.Controls.Add(this.nomeTextBox);
+            this.Controls.Add(this.clienteDataGridView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "FormCliente";
+            this.Text = ".: Cadastro de clientes";
+            this.Load += new System.EventHandler(this.FormCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.clienteDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.locadoraDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox telefoneTextBox;
+        private System.Windows.Forms.TextBox cPFTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox nomeTextBox;
+        private System.Windows.Forms.DataGridView clienteDataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private LocadoraDataSet locadoraDataSet;
+        private System.Windows.Forms.BindingSource clienteBindingSource;
+        private LocadoraDataSetTableAdapters.ClienteTableAdapter clienteTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nomeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cPFDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn telefoneDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ Locadora/FormCliente.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Locadora
+{
+    public partial class FormCliente : Form
+    {
+        public FormCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void limpa_TextBox()
+        {
+            nomeTextBox.Text = "";
+            emailTextBox.Text = "";
+            cPFTextBox.Text = "";
+            telefoneTextBox.Text = "";
+        }
+
+        private void clienteDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            nomeTextBox.Text = clienteDataGridView.CurrentRow.Cells[1].Value.ToString();
+            emailTextBox.Text = clienteDataGridView.CurrentRow.Cells[2].Value.ToString();
+            cPFTextBox.Text = clienteDataGridView.CurrentRow.Cells[3].Value.ToString();
+            telefoneTextBox.Text = clienteDataGridView.CurrentRow.Cells[4].Value.ToString();
+        }
+
+        private void FormCliente_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'locadoraDataSet.Cliente' table. You can move, or remove it, as needed.
+            this.clienteTableAdapter.Fill(this.locadoraDataSet.Cliente);
+
+        }
+
+        private void atualiza_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexao = new SqlConnection("Data Source=DESKTOP-M10J657;Initial Catalog=Locadora;Integrated Security=True");
+
+            SqlCommand comando = new SqlCommand("SELECT * FROM Cliente", conexao);
+            try
+            {
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                SqlDataAdapter adapta = new SqlDataAdapter();
+                DataSet dataset = new DataSet();
+                adapta.SelectCommand = comando;
+                adapta.Fill(dataset);
+                clienteDataGridView.DataSource = dataset;
+                clienteDataGridView.DataMember = dataset.Tables[0].TableName;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+                limpa_TextBox();
+            }
+        }
+
+        private void salva_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexao = new SqlConnection("Data Source=DESKTOP-M10J657;Initial Catalog=Locadora;Integrated Security=True");
+            string inserir = "INSERT INTO Cliente(Nome, Email, CPF, Telefone) VALUES (@Nome, @Email, @CPF, @Telefone)";
+            SqlCommand comando = new SqlCommand(inserir, conexao);
+
+            try
+            {    // insere os dados dos textBox no comando 'inserir'              
+                comando.Parameters.Add(new SqlParameter("@Nome", this.nomeTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@Email", this.emailTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@CPF", this.cPFTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@Telefone", this.telefoneTextBox.Text));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                MessageBox.Show("Salvo com sucesso!");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorre erro: " + ex.Message);
+            }
+            finally
+            {
+                atualiza_Click(sender, e);
+                limpa_TextBox();
+            }
+        }     
+
+        private void altera_Click(object sender, EventArgs e)
+        {
+
+            SqlConnection conexao = new SqlConnection("Data Source=DESKTOP-M10J657;Initial Catalog=Locadora;Integrated Security=True");
+            string alterar = "UPDATE Cliente SET Nome=@Nome, Email=@Email, CPF=@CPF, Telefone=@Telefone WHERE Id=@Id";
+            SqlCommand comando = new SqlCommand(alterar, conexao);
+
+            try
+            {    // insere os dados dos textBox no comando 'inserir'     
+                comando.Parameters.Add(new SqlParameter("@Id", clienteDataGridView.SelectedRows[0].Cells[0].Value.ToString()));
+                comando.Parameters.Add(new SqlParameter("@Nome", this.nomeTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@Email", this.emailTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@CPF", this.cPFTextBox.Text));
+                comando.Parameters.Add(new SqlParameter("@Telefone", this.telefoneTextBox.Text));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                MessageBox.Show("Alterado com sucesso!");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorre erro: " + ex.Message);
+            }
+            finally
+            {
+                atualiza_Click(sender, e);
+                limpa_TextBox();
+            }
+        }
+
+        private void deleta_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexao = new SqlConnection("Data Source=DESKTOP-M10J657;Initial Catalog=Locadora;Integrated Security=True");
+            string deletar = "DELETE FROM Cliente WHERE Id =@Id";
+            SqlCommand comando = new SqlCommand(deletar, conexao);
+
+            try
+            {
+                comando.Parameters.Add(new SqlParameter("@Id", clienteDataGridView.SelectedRows[0].Cells[0].Value.ToString()));
+
+                conexao.Open();
+                comando.ExecuteNonQuery();
+                conexao.Close();
+                MessageBox.Show("Deletado com sucesso!");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Ocorre erro: " + ex.Message);
+            }
+            finally
+            {
+                atualiza_Click(sender, e);
+                limpa_TextBox();
+            }
+        }   
+
+    }
+}
